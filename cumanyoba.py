2faa1a558c3b701b61ff92b81fabc4bff026f4b0
AndraDC <116729974+AndraDC@users.noreply.github.com>
2023-06-15 12:31:10 +0700

    Update cumanyoba.py

--- cumanyoba.py
+++ cumanyoba.py
@@ -85,9 +85,9 @@ def update_plot(attr, old, new):
     plot.title.text = 'Gapminder data for %d' % yr
 
 # Make a slider object: slider
-slider = st.slider('Year', 1970, 2010, (1970,2010))
-start_year = slider[0]
-end_year = slider[1]
+slider = st.slider('Year', 1970, 2010, (1970,2010), 
+                   start_year = slider[0],
+                   end_year = slider[1])
 #slider.on_change('value',update_plot)
 
 # Make dropdown menu for x and y axis
