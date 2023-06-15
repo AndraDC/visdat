0e35ff2652d68cb22ec9df5c52116335d70804fd
AndraDC <116729974+AndraDC@users.noreply.github.com>
2023-06-15 12:57:15 +0700

    Update cumanyoba.py

--- cumanyoba.py
+++ cumanyoba.py
@@ -85,5 +85,5 @@ def update_plot():
     plot.title.text = 'Gapminder data for %d' % yr
 
 # Make a slider object: slider
-slider = st.slider('year', 1970, 2010, (1970,2010), on_change=update_plot)
+slider = st.slider('year',min_value=1970, max_value=1970, value=1970, step=1, on_change=update_plot)
 
